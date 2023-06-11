f5133b5810eae0489b7e1c4a6b14161893d1a90b
Gonue <6017891@naver.com>
2023-06-12 01:16:53 +0900

    feat : alerting-email

---
+++ posts/alerting-ex/src/test/java/com/example/alertingex/AlertingExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alertingex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertingExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
